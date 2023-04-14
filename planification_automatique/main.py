fb90e5df74395b5b8e00d0f72d76b98b300b6aad
Yoan Gabison <yoan.gabison@student-cs.fr>
2023-04-14 13:32:57 +0200

    Add ground actions method

--- planification_automatique/main.py
+++ planification_automatique/main.py
@@ -1,11 +1,12 @@
 import argparse
 from pathlib import Path
 
-from pddl import parse_domain, parse_problem
 import pddl.logic.base as logic
+from pddl import parse_domain, parse_problem
 
 
 def is_applicable(action, state):
+    # print(action.precondition.parts)
     print(type(action.precondition))
     print(state)
     if isinstance(action.precondition, logic.Not):
@@ -20,20 +21,33 @@ def is_applicable(action, state):
     return False
 
 
+def ground_actions(domain, objects: frozenset):
+    grounded_actions: list = []
+    for action in domain.actions:
+        if not action.parameters:
+            grounded_actions.append(action)
+        for parameter in action.parameters:
+            for obj in objects:
+                if obj.type_tags == parameter.type_tags or len(obj.type_tags) == 0:
+                    grounded_actions.append(action)
+    return grounded_actions
+
+
 def main():
-    parser = argparse.ArgumentParser()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
     parser.add_argument("--group", type=int, default=1)
     parser.add_argument("--problem", type=str, default="problem1.pddl")
     parser.add_argument("--domain", type=str, default="domain.pddl")
 
     # Parse args
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
-    folder = Path("planification_automatique/data")
-    group = folder / f"groupe{args.group}"
-    domain = parse_domain(group / args.domain)
-    problem = parse_problem(group / args.problem)
+    data_folder = Path("planification_automatique/data")
+    group: str = f"groupe{args.group}"
+    domain = parse_domain(data_folder / group / args.domain)
+    problem = parse_problem(data_folder / group / args.problem)
     problem.domain = domain
+    print(f"{len(ground_actions(domain, problem.objects)) = }")
     # print(problem.init)
     # print(problem.domain)
     # print(problem.requirements)
@@ -43,8 +57,9 @@ def main():
     for action in problem.domain.actions:
         # print(action.precondition)
         # print(action.effect)
-        # print(action.parameters)
+        # print(f"{action.parameters = }")
         # print(action.name)
+
         # is_applicable(action, problem.init)
         break
 
